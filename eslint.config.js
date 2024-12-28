e219832e854708ff8a1ae3614865dbdfac6e5731
Badol-Hossain1 <badolhossain586.com>
2024-12-28 07:15:31 -0500

    feat: init

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: es2020,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -35,4 +35,4 @@ export default [
       ],
     },
   },
-]
+];
